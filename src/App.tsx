2f663f91065117505d25efc9c975710397c2c91e
Igor Lialko <igorlialko8@gmail.com>
2022-07-04 11:04:16 +0300

    add_lazy_imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-import {BrowserRouter,Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {FC, ReactNode, lazy, Suspense} from "react";
+import TopBarProgress from "react-topbar-progress-indicator";
 
 import Default from "./layouts/Default";
-import HomePage from "./pages/HomePage";
-import ErrorPage from "./pages/ErrorPage";
-import EditPage from "./pages/EditPage";
 
 export default function App() {
+  const HomePage = lazy(() => import("./pages/HomePage"));
+  const EditPage = lazy(() => import("./pages/EditPage"));
+  const ErrorPage = lazy(() => import("./pages/ErrorPage"));
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Default/>} >
-          <Route index element={<HomePage/>} />
-          <Route path=":id" element={<EditPage/>} />
+        <Route path="/" element={<Default/>}>
+          <Route index element={
+            <SuspensedView>
+              <HomePage/>
+            </SuspensedView>
+          }/>
+          <Route path=":id" element={
+            <SuspensedView>
+              <EditPage/>
+            </SuspensedView>
+          }/>
         </Route>
-        <Route path="*" element={<ErrorPage/>} />
+        <Route path="*" element={
+          <SuspensedView>
+            <ErrorPage/>
+          </SuspensedView>
+        }/>
       </Routes>
     </BrowserRouter>
   );
 }
+
+const SuspensedView: FC<{ children: ReactNode }> = ({children}) => {
+  TopBarProgress.config({
+    barColors: {
+      "0": "rgba(82, 34, 140, 1)",
+    },
+    barThickness: 2,
+    shadowBlur: 10,
+  });
+  return <Suspense fallback={<TopBarProgress/>}>{children}</Suspense>;
+};
